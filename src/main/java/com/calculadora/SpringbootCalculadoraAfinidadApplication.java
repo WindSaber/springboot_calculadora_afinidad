bde0391895441598146a0270261399fda0095180
WindSaber <wind.saber@hotmail.com>
2023-01-14 21:14:53 -0600

    Fresh

---
+++ src/main/java/com/calculadora/SpringbootCalculadoraAfinidadApplication.java
@@ -0,0 +1,13 @@
+package com.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCalculadoraAfinidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCalculadoraAfinidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculadora/SpringbootCalculadoraAfinidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCalculadoraAfinidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
